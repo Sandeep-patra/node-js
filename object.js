2684e4a422b47ab1da86ddf7d285b0fde9bc87d5
Sandeep-patra <111298243+Sandeep-patra@users.noreply.github.com>
2022-10-13 19:25:52 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,24 @@
+// Task : Design an Employee Object 
+// 	    //    Arrays,Object,Array of Objects, Object inside Object 
+let employee={
+    "First Name":'Ramchandra',
+    "Last Name":das,
+    "id":'125ad54',
+    "phone":[6000359615,7609992100],
+    "Email":'20mca53@imit.ac.in',
+    "Job Title":'full stack developer',
+    "Department":IT,
+    "Address":{
+        "Permanent address":{
+            "city":"Bhubaneswar",
+            "state":"Odisha",
+            "pin":754202,
+        }
+        "Present address":{
+            "city":'Bangalor',
+            "state":'karnataka',
+            "pin":75123
+
+        }
+    }
+}
